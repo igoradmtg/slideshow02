b07e17a2a871574323aa0abcfba5cc8f080129f4
igoradmtg <85247883+igoradmtg@users.noreply.github.com>
2021-06-07 00:43:22 +0700

    Add files via upload

--- start.py
+++ start.py
@@ -1,8 +1,10 @@
+import os
 from moviepy.editor import *
 from moviepy.editor import TextClip,VideoFileClip, concatenate_videoclips
 dir_name_main = "Z:\\slideshow\\" # The directory contains other directories - Каталог содержит другие каталоги
 dir_name_output = "Z:\\slideshow_out" # Directory for saving files mp4 - Каталог для сохранения файлов mp4
 python_scrypt = "slideshow.py" # Script to start generating the mp4 file - Скрипт для запуска формирования файла mp4
+Video_type = 4 #  1 - 720х480, 2 - 854x480, 3 - 1280x720, 4 - 1920x1080,  5 - 3840x2160 - Разрешение для видео
 def save_file_busy(fname):
     f = open(fname,"a")
     f.write("busy")
@@ -10,6 +12,9 @@ def save_file_busy(fname):
 
 def main() : 
     global dir_name_main,dir_name_output,python_scrypt
+    # Создать каталог если он не существует
+    if os.path.isdir(dir_name_output) == False:
+        os.mkdir(dir_name_output)
     dir_names = os.listdir(dir_name_main) # We read the contents of the directory - Читаем содержимое каталога
     is_found_dir = False # Found directory attribute - Признак найденного каталога 
     fname_mp4 = ""
@@ -33,7 +38,7 @@ def main() :
 
     if is_found_dir :
         save_file_busy(fname_busy) # Save the flag is busy - Сохранение флага занято
-        str_exec = python_scrypt + " "+dir_fullname+" "+fname_mp4 +" " + dir_name_output # form a string for execution - формируем строку для выполнения
+        str_exec = python_scrypt + " "+dir_fullname+" "+fname_mp4 +" " + dir_name_output + " " + str(Video_type) # form a string for execution - формируем строку для выполнения
         print("Execute ",str_exec) # Displaying - Выводим на экран
         os.system(str_exec) # Run the script for execution - Запускаем скрипт на выполнение
   
